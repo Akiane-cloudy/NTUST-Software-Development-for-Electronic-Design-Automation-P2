3617a3ad583c791acce6dfb79e7f6dd8f6f6db22
Akiane-cloudy <josker6969@gmail.com>
2025-04-09 01:37:49 +0800

    Modify: Modified the parameters of addCel function to accept xc

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -1,16 +1,16 @@
 #include "../include/Cluster.hpp"
 
-Cluster::Cluster(int _ec, int _wc, int _qc) {
+Cluster::Cluster(int _ec, int _wc, int _qc, int _xc) {
     ec = _ec;
     wc = _wc;
     qc = _qc;
-    xc = 0;
+    xc = _xc;
 }
 
-void Cluster::addCel(Cell* cell) {
+void Cluster::addCel(Cell* cell, int xc) {
     cells.push_back(cell);
 
     ec += cell->weight;
-    qc += cell->weight * (cell->x_globalPlacement - wc);
+    qc += cell->weight * (xc - wc);
     wc += cell->width;
 }
